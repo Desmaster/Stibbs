6648d1f33c7a13044caed0f2b2b477b3a0f6bfe8
Timon de Groot <tdegroot96>
2012-09-18 14:00:51 +0200

    Added an Input Handler

--- src/com/github/desmaster/input/InputHandler.java
+++ src/com/github/desmaster/input/InputHandler.java
@@ -1,21 +1,42 @@
 package com.github.desmaster.input;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
 
 import com.github.desmaster.main.Stibbs;
 
 public class InputHandler {
 
 	Stibbs game;
+	double movementSpeed = 2.75;
+	int gameWidth;
 	
 	public InputHandler(Stibbs game) {
 		this.game = game;
+		gameWidth = Display.getDisplayMode().getWidth();
 	}
 	
 	public void tick() {
 		if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
 			game.finished = true;
 		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+			game.x -= movementSpeed;
+		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+			game.x += movementSpeed;
+		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+			game.y += movementSpeed;
+		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+			game.y -= movementSpeed;
+		}
+		
 	}
 	
 }
